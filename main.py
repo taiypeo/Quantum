df141ebdd7a00c23f286fffad6e1e1938a4e0d84
QwertygidQ <Qwertygid@gmail.com>
2019-02-25 03:50:38 +0300

    Added one more example

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from quantum.ConstGates import *
 
 
 if __name__ == '__main__':
+    # Random 3-qubit circuit demonstation
+
     out = Circuit(3).add_column([
         H, I2, H
     ]).add_column([
@@ -12,3 +14,33 @@ if __name__ == '__main__':
     ]).reshape(-1, 1))
 
     print(out)
+    '''
+    [[0.5]
+    [0.5]
+    [0. ]
+    [0. ]
+    [0. ]
+    [0. ]
+    [0.5]
+    [0.5]]
+    '''
+
+    print()
+    #=======================================================
+    # Bell state simulation demonstration
+
+    out = Circuit(2).add_column([
+        H, I2
+    ]).add_column([
+        CNOT
+    ]).calculate_probabilities(np.array([
+        1, 0, 0, 0
+    ]).reshape(-1, 1))
+
+    print(out)
+    '''
+    [[0.70710678]
+    [0.        ]
+    [0.        ]
+    [0.70710678]]
+    '''
